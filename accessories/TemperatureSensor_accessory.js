337a20ad5c7a2ef80f4f3440ce847d4e98ed2dd6
ADalichuk <alextropa@gmail.com>
2018-02-17 14:02:53 +0200

    Update TemperatureSensor_accessory.js

--- accessories/TemperatureSensor_accessory.js
+++ accessories/TemperatureSensor_accessory.js
@@ -2,52 +2,79 @@ var Accessory = require('../').Accessory;
 var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
+var sensorLib = require('node-dht-sensor');
 
-// here's a fake temperature sensor device that we'll expose to HomeKit
-var FAKE_SENSOR = {
-  currentTemperature: 50,
+var sensorType = 22; // 11 for DHT11, 22 for DHT22 and AM2302
+var sensorPin  = 17;  // The GPIO pin number for sensor signal
+if (!sensorLib.initialize(sensorType, sensorPin)) {
+    console.warn('Failed to initialize sensor');
+    process.exit(1);
+}
+
+// here's a temperature sensor device that we'll expose to HomeKit
+var DHT_SENSOR = {
+  currentTemperature: 20,
+  currentHumidity: 30,
   getTemperature: function() { 
     console.log("Getting the current temperature!");
-    return FAKE_SENSOR.currentTemperature;
+    return DHT_SENSOR.currentTemperature;
   },
-  randomizeTemperature: function() {
-    // randomize temperature to a value between 0 and 100
-    FAKE_SENSOR.currentTemperature = Math.round(Math.random() * 100);
+  getHumidity: function() { 
+    console.log("Getting the current humidity!");
+    return DHT_SENSOR.currentHumidity;
+  },
+  read() {
+    DHT_SENSOR.currentTemperature = sensorLib.read().temperature.toFixed(1));
+    DHT_SENSOR.currentHumidity = sensorLib.read().humidity.toFixed(1));
   }
 }
 
-
-// Generate a consistent UUID for our Temperature Sensor Accessory that will remain the same
+// Generate a consistent UUID for our Temperature&Humidity Sensor Accessory that will remain the same
 // even when restarting our server. We use the `uuid.generate` helper function to create
-// a deterministic UUID based on an arbitrary "namespace" and the string "temperature-sensor".
-var sensorUUID = uuid.generate('hap-nodejs:accessories:temperature-sensor');
+// a deterministic UUID based on an arbitrary "namespace" and the string "temperature-sensor", "humidity-sensor".
 
-// This is the Accessory that we'll return to HAP-NodeJS that represents our fake lock.
-var sensor = exports.accessory = new Accessory('Temperature Sensor', sensorUUID);
+// This is the Accessory that we'll return to HAP-NodeJS.
+var temperatureSensor = exports.accessory = new Accessory('Temperature Sensor', uuid.generate('hap-nodejs:accessories:temperature-sensor'));
+var humiditySensor = exports.accessory = new Accessory('Humidity Sensor', uuid.generate('hap-nodejs:accessories:humidity-sensor'));
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-sensor.username = "C1:5D:3A:AE:5E:FA";
-sensor.pincode = "031-45-154";
+temperatureSensor.username = "C1:5D:3A:AE:5E:FA";
+temperatureSensor.pincode = "031-45-154";
+humiditySensor.username = "C1:5D:3A:AE:5E:FA";
+humiditySensor.pincode = "031-45-154";
 
-// Add the actual TemperatureSensor Service.
+// Add the actual TemperatureSensor&HumiditySensor Service.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-sensor
+temperatureSensor
   .addService(Service.TemperatureSensor)
   .getCharacteristic(Characteristic.CurrentTemperature)
   .on('get', function(callback) {
     
     // return our current value
-    callback(null, FAKE_SENSOR.getTemperature());
+    callback(null, DHT_SENSOR.getTemperature());
   });
 
-// randomize our temperature reading every 3 seconds
+humiditySensor
+  .addService(Service.HumiditySensor)
+  .getCharacteristic(Characteristic.CurrentHumidity)
+  .on('get', function(callback) {
+    
+    // return our current value
+    callback(null, DHT_SENSOR.getHumidity());
+  });
+  
+// temperature reading every 3 seconds
 setInterval(function() {
   
-  FAKE_SENSOR.randomizeTemperature();
+  DHT_SENSOR.read();
   
   // update the characteristic value so interested iOS devices can get notified
-  sensor
+  temperatureSensor
     .getService(Service.TemperatureSensor)
-    .setCharacteristic(Characteristic.CurrentTemperature, FAKE_SENSOR.currentTemperature);
+    .setCharacteristic(Characteristic.CurrentTemperature, DHT_SENSOR.currentTemperature);
+    
+  humiditySensor
+    .getService(Service.HumiditySensor)
+    .setCharacteristic(Characteristic.CurrentHumidity, DHT_SENSOR.currentHumidity);
   
 }, 3000);
