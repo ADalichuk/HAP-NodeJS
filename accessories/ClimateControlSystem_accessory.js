6a395bdcbcb404534c020663d3ef3de4f9c2e5e7
oleksii.dalichuk <oleksii.dalichuk@envisiontec.de>
2018-02-25 13:40:56 +0200

    Added two DHT sensors for in and out air flows

--- accessories/ClimateControlSystem_accessory.js
+++ accessories/ClimateControlSystem_accessory.js
@@ -2,6 +2,55 @@ var Accessory = require('../').Accessory;
 var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
+var sensorLib1 = require('node-dht-sensor');
+var sensorLib2 = require('node-dht-sensor');
+
+var sensorType = 22; // 11 for DHT11, 22 for DHT22 and AM2302
+var sensorPin1  = 17;  // The GPIO pin number for sensor signal
+var sensorPin2  = 4;  // The GPIO pin number for sensor signal
+if (!sensorLib1.initialize(sensorType, sensorPin1)) {
+    console.warn('Failed to initialize dht sensor 1');
+    process.exit(1);
+}
+if (!sensorLib2.initialize(sensorType, sensorPin2)) {
+    console.warn('Failed to initialize dht sensor 2');
+    process.exit(1);
+}
+
+// here's a temperature sensor device that we'll expose to HomeKit
+var DHT_SENSOR_1 = {
+  currentTemperature: 20,
+  currentHumidity: 30,
+  getTemperature: function() { 
+    console.log("Getting the current temperature!");
+    return DHT_SENSOR_1.currentTemperature;
+  },
+  getHumidity: function() { 
+    console.log("Getting the current humidity!");
+    return DHT_SENSOR_1.currentHumidity;
+  },
+  read() {
+    DHT_SENSOR_1.currentTemperature = sensorLib1.read().temperature.toFixed(1);
+    DHT_SENSOR_1.currentHumidity = sensorLib1.read().humidity.toFixed(1);
+  }
+}
+
+var DHT_SENSOR_2 = {
+  currentTemperature: 20,
+  currentHumidity: 30,
+  getTemperature: function() { 
+    console.log("Getting the current temperature!");
+    return DHT_SENSOR_2.currentTemperature;
+  },
+  getHumidity: function() { 
+    console.log("Getting the current humidity!");
+    return DHT_SENSOR_2.currentHumidity;
+  },
+  read() {
+    DHT_SENSOR_2.currentTemperature = sensorLib1.read().temperature.toFixed(1);
+    DHT_SENSOR_2.currentHumidity = sensorLib1.read().humidity.toFixed(1);
+  }
+}
 
 //In This example we create an Airconditioner Accessory that Has a Thermostat linked to a Fan Service.
 //For example, I've also put a Light Service that should be hidden to represent a light in the closet that is part of the AC. It is to show how to hide services.
@@ -11,33 +60,37 @@ var uuid = require('../').uuid;
 var ACTest_data = {
   fanPowerOn: false,
   rSpeed: 100,
-  CurrentHeatingCoolingState: 1,
-  TargetHeatingCoolingState: 1,
-  CurrentTemperature: 33,
-  TargetTemperature: 32,
-  TemperatureDisplayUnits: 1,
-  LightOn: false
+
 }
 
+var CSS_Controller = {
+  name: 'Climate Control System', //name of accessory
+  UUID: uuid.generate('hap-nodejs:accessories:airconditioner'),
+  pincode: "031-45-154",
+  username: "1A:2B:3C:4D:5E:FF", // MAC like address used by HomeKit to differentiate accessories. 
+  manufacturer: "Aleksey Dalichuk", //manufacturer (optional)
+  model: "v1.0", //model (optional)
+  serialNumber: "A12S345KGB", //serial number (optional)
+}
 // This is the Accessory that we'll return to HAP-NodeJS that represents our fake fan.
-var ACTest = exports.accessory = new Accessory('Climate Control System', uuid.generate('hap-nodejs:accessories:airconditioner'));
+var ACTest = exports.accessory = new Accessory(CSS_Controller.name, CSS_Controller.UUID);
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-ACTest.username = "1A:2B:3C:4D:5E:FF";
-ACTest.pincode = "031-45-154";
+ACTest.username = CSS_Controller.username;
+ACTest.pincode = CSS_Controller.pincode;
 
 // set some basic properties (these values are arbitrary and setting them is optional)
 ACTest
   .getService(Service.AccessoryInformation)
-  .setCharacteristic(Characteristic.Manufacturer, "Sample Company")
+  .setCharacteristic(Characteristic.Manufacturer, CSS_Controller.manufacturer)
 
 // listen for the "identify" event for this Accessory
 ACTest.on('identify', function(paired, callback) {
-  console.log("Fan Identified!");
+  console.log("Climate Control System Identified!");
   callback(); // success
 });
 
-// Add the actual Fan Service and listen for change events from iOS.
+// Add the actual Service and listen for change events from iOS.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
 
 var FanService = ACTest.addService(Service.Fan, "Blower") // services exposed to the user should have "names" like "Fake Light" for us
@@ -79,71 +132,62 @@ FanService.addCharacteristic(Characteristic.RotationSpeed)
     callback();
   })
 
-var ThermostatService = ACTest.addService(Service.Thermostat,"Thermostat");
-ThermostatService.addLinkedService(FanService);
+// This is the Accessory that we'll return to HAP-NodeJS.
+var T_IN_SERVICE = ACTest.addService(Service.TemperatureSensor, "T In")
+var H_IN_SERVICE = ACTest.addService(Service.HumiditySensor, "H In")
 
-ThermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+var T_OUT_SERVICE = ACTest.addService(Service.TemperatureSensor, "T Out")
+var H_OUT_SERVICE = ACTest.addService(Service.HumiditySensor, "H Out")
 
+// Add the actual TemperatureSensor&HumiditySensor Service.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+T_IN_SERVICE
+  .getCharacteristic(Characteristic.CurrentTemperature)
   .on('get', function(callback) {
-    callback(null, ACTest_data.CurrentHeatingCoolingState);
-  })
-  .on('set',function(value, callback) {
-      ACTest_data.CurrentHeatingCoolingState=value;
-      console.log( "Characteristic CurrentHeatingCoolingState changed to %s",value);
-      callback();
-    });
-
- ThermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState)
-  .on('get', function(callback) {
-    callback(null, ACTest_data.TargetHeatingCoolingState);
-  })
-  .on('set',function(value, callback) {
-      ACTest_data.TargetHeatingCoolingState=value;
-      console.log( "Characteristic TargetHeatingCoolingState changed to %s",value);
-      callback();
-    });
-
- ThermostatService.getCharacteristic(Characteristic.CurrentTemperature)
-  .on('get', function(callback) {
-    callback(null, ACTest_data.CurrentTemperature);
-  })
-  .on('set',function(value, callback) {
-      ACTest_data.CurrentTemperature=value;
-      console.log( "Characteristic CurrentTemperature changed to %s",value);
-      callback();
-    });
-
- ThermostatService.getCharacteristic(Characteristic.TargetTemperature)
-  .on('get', function(callback) {
-    callback(null, ACTest_data.TargetTemperature);
-  })
-  .on('set',function(value, callback) {
-      ACTest_data.TargetTemperature=value;
-      console.log( "Characteristic TargetTemperature changed to %s",value);
-      callback();
-    });
-
- ThermostatService.getCharacteristic(Characteristic.TemperatureDisplayUnits)
-  .on('get', function(callback) {
-    callback(null, ACTest_data.TemperatureDisplayUnits);
-  })
-  .on('set',function(value, callback) {
-      ACTest_data.TemperatureDisplayUnits=value;
-      console.log( "Characteristic TemperatureDisplayUnits changed to %s",value);
-      callback();
-    });
     
+    // return our current value
+    callback(null, DHT_SENSOR_1.getTemperature());
+  });
+
+H_IN_SERVICE
+  .getCharacteristic(Characteristic.CurrentRelativeHumidity )
+  .on('get', function(callback) {
+    
+    // return our current value
+    callback(null, DHT_SENSOR_1.getHumidity());
+  });
+  
+T_OUT_SERVICE
+  .getCharacteristic(Characteristic.CurrentTemperature)
+  .on('get', function(callback) {
+    
+    // return our current value
+    callback(null, DHT_SENSOR_2.getTemperature());
+  });
+
+H_OUT_SERVICE
+  .getCharacteristic(Characteristic.CurrentRelativeHumidity )
+  .on('get', function(callback) {
+    
+    // return our current value
+    callback(null, DHT_SENSOR_2.getHumidity());
+  });
+  
+// temperature reading every 3 seconds
+setInterval(function() {
+  
+  DHT_SENSOR_1.read();
+  DHT_SENSOR_2.read();
+  
+  // update the characteristic value so interested iOS devices can get notified
+  T_IN_SERVICE.setCharacteristic(Characteristic.CurrentTemperature, DHT_SENSOR_1.currentTemperature);
+  H_IN_SERVICE.setCharacteristic(Characteristic.CurrentRelativeHumidity, DHT_SENSOR_1.currentHumidity);
+  
+  T_OUT_SERVICE.setCharacteristic(Characteristic.CurrentTemperature, DHT_SENSOR_2.currentTemperature);
+  H_OUT_SERVICE.setCharacteristic(Characteristic.CurrentRelativeHumidity, DHT_SENSOR_2.currentHumidity);
+
+}, 3000);
 
 
-var LightService = ACTest.addService(Service.Lightbulb, 'AC Light');
-LightService.getCharacteristic(Characteristic.On)
-    .on('get', function(callback) {
-      callback(null, ACTest_data.LightOn);
-    })
-    .on('set', function(value, callback) {
-      ACTest_data.LightOn=value;
-      console.log( "Characteristic Light On changed to %s",value);
-      callback();
-    });
-LightService.setHiddenService(true);
-ACTest.setPrimaryService(ThermostatService);
+//LightService.setHiddenService(true);
+//ACTest.setPrimaryService(ThermostatService);
