79ad9244990ac42e89565cc70122e4f8b8d5d29f
ADalichuk <alextropa@gmail.com>
2018-03-15 01:12:28 +0200

    Update AirQuality_accessory.js

--- accessories/AirQuality_accessory.js
+++ accessories/AirQuality_accessory.js
@@ -8,6 +8,8 @@ var sleep = require('sleep');
 // initialize UART and CO2 sensor
 var uart = new SerialPort('/dev/serial0', {baudRate: 9600});
 sleep.sleep(1);
+uart.open();
+sleep.sleep(1);
 uart.write("\xff\x01\x99\x00\x00\x00\x07\xd0\x8f");
 sleep.msleep(100);
 uart.read();
