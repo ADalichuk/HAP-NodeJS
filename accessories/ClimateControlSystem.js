76c2894f40fde75c5beba48f9995a22da45a2e26
ADalichuk <alextropa@gmail.com>
2018-03-18 01:30:32 +0200

    Update ClimateControlSystem.js

--- accessories/ClimateControlSystem.js
+++ accessories/ClimateControlSystem.js
@@ -33,48 +33,6 @@ cssAccessory.on('identify', function(paired, callback) {
   callback(); // success
 });
 
-// Add the actual Fan Service and listen for change events from iOS.
-// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-
-var FanService = cssAccessory.addService(Service.Fan, "Fan") 
-FanService.getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
-    console.log("Fan Power Changed To "+value);
-    cssData.fanPowerOn=value
-    callback(); // Our fake Fan is synchronous - this value has been successfully set
-  });
-
-// We want to intercept requests for our current power state so we can query the hardware itself instead of
-// allowing HAP-NodeJS to return the cached Characteristic.value.
-FanService.getCharacteristic(Characteristic.On)
-  .on('get', function(callback) {
-
-    // this event is emitted when you ask Siri directly whether your fan is on or not. you might query
-    // the fan hardware itself to find this out, then call the callback. But if you take longer than a
-    // few seconds to respond, Siri will give up.
-
-    var err = null; // in case there were any problems
-
-    if (cssData.fanPowerOn) {
-      callback(err, true);
-    }
-    else {
-      callback(err, false);
-    }
-  });
-
-
-// also add an "optional" Characteristic for spped
-FanService.addCharacteristic(Characteristic.RotationSpeed)
-  .on('get', function(callback) {
-    callback(null, cssData.rSpeed);
-  })
-  .on('set', function(value, callback) {
-    console.log("Setting fan rSpeed to %s", value);
-    cssData.rSpeed=value
-    callback();
-  })
-
 var ThermostatService = cssAccessory.addService(Service.Thermostat,"Thermostat");
 ThermostatService.addLinkedService(FanService);
 
@@ -335,7 +293,125 @@ setInterval(function() {
     .setCharacteristic(Characteristic.AirQuality, airQuality)
     .setCharacteristic(Characteristic.CarbonDioxideLevel, CO2_SENSOR.getLevel());
   
-}, 3000);    
+}, 3000);
+
+
+
+
+
+
+var Gpio = require('pigpio').Gpio;
+var GPIOOnOff = require('onoff').Gpio;
+
+// here's a fake hardware device that we'll expose to HomeKit
+var FAKE_FAN = {
+  powerOn: false,
+  rSpeed: 100,
+  pwmControl: new Gpio(12, {mode: Gpio.OUTPUT}),
+  powerOnRelay: new GPIOOnOff(26, 'out'),
+  speed_1_Relay: new GPIOOnOff(19, 'out'),
+  speed_2_Relay: new GPIOOnOff(13, 'out'),
+  speed_3_Relay: new GPIOOnOff(6, 'out'),
+  setPowerOn: function(on) {
+    if(on){
+      //put your code here to turn on the fan
+      FAKE_FAN.powerOn = on;
+    }
+    else{
+      //put your code here to turn off the fan
+      FAKE_FAN.powerOn = on;
+    }
+  },
+  setSpeed: function(value) {
+    console.log("Setting fan rSpeed to %s", value);
+    FAKE_FAN.rSpeed = value;
+    //put your code here to set the fan to a specific value
+  },
+  identify: function() {
+    //put your code here to identify the fan
+    console.log("Fan Identified!");
+  }
+}
+
+// Add the actual Fan Service and listen for change events from iOS.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+
+var fan = cssAccessory.addService(Service.Fan, "Fan") 
+
+// listen for the "identify" event for this Accessory
+fan.on('identify', function(paired, callback) {
+  FAKE_FAN.identify();
+  callback(); // success
+});
+
+// Add the actual Fan Service and listen for change events from iOS.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+fan
+  .addService(Service.Fan, "Fan") // services exposed to the user should have "names" like "Fake Light" for us
+  .getCharacteristic(Characteristic.On)
+  .on('set', function(value, callback) {
+    FAKE_FAN.setPowerOn(value);
+    callback(); // Our fake Fan is synchronous - this value has been successfully set
+  });
+
+// We want to intercept requests for our current power state so we can query the hardware itself instead of
+// allowing HAP-NodeJS to return the cached Characteristic.value.
+fan
+  .getService(Service.Fan)
+  .getCharacteristic(Characteristic.On)
+  .on('get', function(callback) {
+
+    // this event is emitted when you ask Siri directly whether your fan is on or not. you might query
+    // the fan hardware itself to find this out, then call the callback. But if you take longer than a
+    // few seconds to respond, Siri will give up.
+
+    var err = null; // in case there were any problems
+
+    if (FAKE_FAN.powerOn) {
+      callback(err, true);
+    }
+    else {
+      callback(err, false);
+    }
+  });
+
+// also add an "optional" Characteristic for spped
+fan
+  .getService(Service.Fan)
+  .addCharacteristic(Characteristic.RotationSpeed)
+  .on('get', function(callback) {
+    callback(null, FAKE_FAN.rSpeed);
+  })
+  .on('set', function(value, callback) {
+    FAKE_FAN.setSpeed(value);
+    callback();
+  })
+
+// Setup fans speed control 
+setInterval(function () {
+  //FAKE_FAN.pwmControl.pwmWrite(Math.floor(FAKE_FAN.rSpeed * 255 / 100) * !FAKE_FAN.powerOn);
+  FAKE_FAN.pwmControl.pwmWrite(255 * !FAKE_FAN.powerOn);
+  FAKE_FAN.powerOnRelay.writeSync(FAKE_FAN.powerOn ? 0 : 1);
+  
+  // turn on corresponding relay according to fan speed value
+   switch (true) {        
+     case (FAKE_FAN.rSpeed < 33):
+         FAKE_FAN.speed_3_Relay.writeSync(1);
+         FAKE_FAN.speed_2_Relay.writeSync(1);
+         FAKE_FAN.speed_1_Relay.writeSync(0);
+         break;
+     case (FAKE_FAN.rSpeed >= 33 && FAKE_FAN.rSpeed < 66 ):
+         FAKE_FAN.speed_3_Relay.writeSync(1);
+         FAKE_FAN.speed_1_Relay.writeSync(1);
+         FAKE_FAN.speed_2_Relay.writeSync(0);
+         break;
+     case (FAKE_FAN.rSpeed >= 66):
+         FAKE_FAN.speed_2_Relay.writeSync(1);
+         FAKE_FAN.speed_1_Relay.writeSync(1);
+         FAKE_FAN.speed_3_Relay.writeSync(0);
+         break;
+   }
+}, 500);// update fan speed once per 500 milliseconds
     
 
 cssAccessory.setPrimaryService(ThermostatService);
