26a5fc8a6066fe2706c6f2e3926d027896853b05
ADalichuk <ADalichuk@github.com>
2018-03-18 23:26:18 +0000

    working version

--- accessories/modules/CarbonDioxideSensor.js
+++ accessories/modules/CarbonDioxideSensor.js
@@ -7,7 +7,7 @@ var sleep = require('sleep');
 class CarbonDioxideSensor {
   constructor(options) {
       
-    this.service = Service("CO2", Service.AirQualitySensor.UUID);
+    this.service = new Service("CO2", Service.AirQualitySensor.UUID);
 
     this.setRangeCmd = Buffer.from([0xff, 0x01, 0x99, 0x00, 0x00, 0x00, 0x07, 0xd0, 0x8f]);
     this.readDataCmd = Buffer.from([0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
@@ -22,7 +22,7 @@ class CarbonDioxideSensor {
   initialize() {
     this.uart = new SerialPort('/dev/serial0', {baudRate: 9600});
     sleep.sleep(2);
-    uart.write(this.setRangeCmd);
+    this.uart.write(this.setRangeCmd);
     sleep.msleep(100);
     this.uart.read(9);
     
@@ -45,9 +45,9 @@ class CarbonDioxideSensor {
   read() {
     this.uart.write(this.readDataCmd);
     sleep.msleep(200);
-    var response=this.uart.read();
+    var response = this.uart.read();
     
-    if (response[0] == 0xff && response[1] == 0x86){
+    if (response && response[0] == 0xff && response[1] == 0x86){
         this.currentLevel = response[2]*256 + response[3];
         this.service
             .setCharacteristic(Characteristic.AirQuality, this.getAirQuality())
